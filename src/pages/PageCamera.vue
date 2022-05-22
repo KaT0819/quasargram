c22120410641354b359392511c8102af79556619
Katsuyuki Toki <katuyuki.toki@gmail.com>
2022-05-22 22:06:29 +0900

    写真投稿画面レイアウト

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,11 +1,34 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/boy-avatar.png" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn color="grey-10" icon="eva-camera" size="lg" round />
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="text" class="col col-sm-6" label="Caption" dense />
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-input v-model="text" class="col col-sm-6" label="Location" dense />
+        <template>
+          <q-btn round dense flat icon="eva-pin-outline" />
+        </template>
+      </div>
+      <div class="row justify-center q-ma-md">
+        <q-btn color="primary" label="投稿する" rounded unelevated />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageCamera'
-}
+  name: "PageCamera",
+};
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
